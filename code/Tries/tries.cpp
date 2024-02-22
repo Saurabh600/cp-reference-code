c1f15373863ec1320f51bb745770f5f7c90aab64
Saurabh Chaudhary <saurabh.ch169@gmail.com>
2024-02-22 12:24:10 +0530

    trie ds updated with hash map

--- code/Tries/tries.cpp
+++ code/Tries/tries.cpp
@@ -1,67 +1,42 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-struct Node {
-    vector<Node *> links;
-    bool flag;
-
-    Node() {
-        links.resize(128);
-        flag = false;
-    }
-
-    bool containsKey(char ch) {
-        return links[ch] != nullptr;
-    }
-    Node *get(char ch) {
-        return links[ch];
-    }
-    void put(char ch, Node *node) {
-        links[ch] = node;
-    }
-    void setEnd() {
-        flag = true;
-    }
-    bool isEnd() {
-        return flag;
-    }
-};
-
 class Trie {
-    Node *root;
+    struct Node {
+        unordered_map<char, Node*> mp;
+        bool isEnd = false;
+    };
+    Node* root;
 
     public:
     Trie() {
-        root = new Node();
+        this->root = new Node();
     }
 
     void insert(string word) {
         auto node = root;
-
         for (char ch : word) {
-            if (!node->containsKey(ch)) {
-                node->put(ch, new Node());
-            }
-            node = (node->get(ch));
+            if (!node->mp.count(ch)) node->mp[ch] = new Node();
+            node = node->mp[ch];
         }
-        node->setEnd();
+        node->isEnd = true;
     }
 
     bool search(string word) {
         auto node = root;
         for (char ch : word) {
-            if (!node->containsKey(ch)) return false;
-            node = node->get(ch);
+            if (!node->mp.count(ch)) return false;
+            node = node->mp[ch];
         }
 
-        return node->isEnd();
+        return node->isEnd;
     }
 
     bool startsWith(string prefix) {
         auto node = root;
         for (char ch : prefix) {
-            if (!node->containsKey(ch)) return false;
-            node = node->get(ch);
+            if (!node->mp.count(ch)) return false;
+            node = node->mp[ch];
         }
 
         return true;
