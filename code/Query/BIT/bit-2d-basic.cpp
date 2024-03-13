0d65546006862c5ae2d510fc6faefcacfbb25a37
Saurabh Chaudhary <saurabh.ch169@gmail.com>
2024-03-13 14:10:49 +0530

    bit assert removed

--- code/Query/BIT/bit-2d-basic.cpp
+++ code/Query/BIT/bit-2d-basic.cpp
@@ -11,7 +11,6 @@ class FenwickTree2D {
   FenwickTree2D(const std::vector<vector<M>>& v) {
     n = v.size();
     m = v[0].size();
-    assert(n > 0 && m > 0);
 
     bit.resize(n + 1, std::vector<T>(m + 1, 0));
 
@@ -23,14 +22,12 @@ class FenwickTree2D {
   }
 
   FenwickTree2D(int n, int m) {
-    assert(n > 0 && m > 0);
     this->n = n;
     this->m = m;
     bit.resize(n + 1, vector<T>(m + 1, 0));
   }
 
   T get(int x, int y) {
-    assert(x > 0 && x <= n && y > 0 && y <= m);
     T sum = 0;
     for (int i = x; i > 0; i -= (i & -i)) {
       for (int j = y; j > 0; j -= (j & -j)) {
@@ -41,7 +38,6 @@ class FenwickTree2D {
   }
 
   void update(int x, int y, T val) {
-    assert(x > 0 && x <= n && y > 0 && y <= m);
     for (int i = x; i <= n; i += (i & -i)) {
       for (int j = y; j <= m; j += (j & -j)) {
         bit[i][j] += val;
@@ -50,8 +46,6 @@ class FenwickTree2D {
   }
 
   T get(int x1, int y1, int x2, int y2) {
-    assert(x1 > 0 && x1 <= n && y1 > 0 && y1 <= m);
-    assert(x2 > 0 && x2 <= n && y2 > 0 && y2 <= m);
     return get(x2, y2) - get(x2, y1 - 1) - get(x1 - 1, y2) + get(x1 - 1, y1 - 1);
   }
 };
