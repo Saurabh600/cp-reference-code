231f74a44a5d7644e69b6a12ae53421e03791bef
Saurabh Chaudhary <saurabh.ch169@gmail.com>
2024-03-23 00:13:25 +0530

    modular class updated

--- README.md
+++ README.md
@@ -4,42 +4,16 @@
 
 - [math](./code/Math/)
 
-  - [factors](./code/Math/factors.cpp)
-  - [modpow](./code/Math/modpow.cpp)
-  - [prime](./code/Math/prime.cpp)
-  - [factorial, inverse factorial & ncr](./code/Math/factorial.cpp)
-
-- [Bit Masking](./code/Bit-Masking/)
-
-  - [cout set bits till N](./code/Bit-Masking/cout-set-bits.cpp)
-
-- [Tries](./code/Tries/)
-
-  - [tries](./code/Tries/tries.cpp)
+- [Query](./code/Query/)
+  - [Binary Indexed Tree](./code/Query/BIT/)
+  - [Segment Tree](./code/Query/SegmentTree/)
 
 - [Graph](./code/Graph/)
-
   - [Cycle In Graph](./code/Graph/cycle-in-graph/)
-
-    - [cycle in directed graph](./code/Graph/cycle-in-graph/directed.cpp)
-    - [cycle in undirected graph](./code/Graph/cycle-in-graph/undirected.cpp)
-
   - [Topo Sort](./code/Graph/topo-sort/)
-
-    - [Kahns Algo](./code/Graph/topo-sort/kahns-algo.cpp)
-    - [topo sort dfs](./code/Graph/topo-sort/dfs.cpp)
-
   - [Shortest Path](./code/Graph/shortest-path/)
-
-    - [In dag](./code/Graph/shortest-path/shortest-path-in-dag.cpp)
-    - [dijkstra algo](./code/Graph/shortest-path/dijkstra-algo.cpp)
-    - [bellman ford algo](./code/Graph/shortest-path/bellman-ford.cpp)
-    - [floyd warshal algo](./code/Graph/shortest-path/floyd-warshall-algo.cpp)
-
   - [DSU](./code/Graph/dsu/)
-
-    - [Disjoint Set](./code/Graph/dsu/disjoint-set.cpp)
-
   - [MST](./code/Graph/mst/)
 
-    - [prims algo](./code/Graph/mst/prims-algo.cpp)
+- [Bit Masking](./code/Bit-Masking/)
+- [Tries](./code/Tries/)
--- code/Math/modular.cpp
+++ code/Math/modular.cpp
@@ -1,28 +1,44 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-const int64_t MOD = 1000000007;
+template <int MOD=998'244'353>
+struct Modular {
+  int value;
+  static const int MOD_value = MOD;
 
-int64_t mpow(int64_t base, int64_t exponent, int64_t modular = MOD) {
-  base %= modular;
-  int64_t result = 1;
-  while (exponent > 0) {
-    if (exponent & 1) result = (result * base) % modular;
-    base = (base * base) % modular;
-    exponent >>= 1;
+  Modular(long long v = 0) { value = v % MOD; if (value < 0) value += MOD;}
+  Modular(long long a, long long b) : value(0){ *this += a; *this /= b;}
+
+  Modular& operator+=(Modular const& b) {value += b.value; if (value >= MOD) value -= MOD; return *this;}
+  Modular& operator-=(Modular const& b) {value -= b.value; if (value < 0) value += MOD;return *this;}
+  Modular& operator*=(Modular const& b) {value = (long long)value * b.value % MOD;return *this;}
+
+  friend Modular mexp(Modular a, long long e) {
+    Modular res = 1; while (e) { if (e&1) res *= a; a *= a; e >>= 1; }
+    return res;
   }
+  friend Modular inverse(Modular a) { return mexp(a, MOD - 2); }
 
-  return result;
-}
+  Modular& operator/=(Modular const& b) { return *this *= inverse(b); }
+  friend Modular operator+(Modular a, Modular const b) { return a += b; }
+  friend Modular operator-(Modular a, Modular const b) { return a -= b; }
+  friend Modular operator-(Modular const a) { return 0 - a; }
+  friend Modular operator*(Modular a, Modular const b) { return a *= b; }
+  friend Modular operator/(Modular a, Modular const b) { return a /= b; }
+  friend std::ostream& operator<<(std::ostream& os, Modular const& a) {return os << a.value;}
+  friend bool operator==(Modular const& a, Modular const& b) {return a.value == b.value;}
+  friend bool operator!=(Modular const& a, Modular const& b) {return a.value != b.value;}
+};
 
-int64_t add(int64_t n1, int64_t n2, int64_t modular = MOD) {
-  return (n1 + n2) % modular;
-}
-
-int64_t subs(int64_t n1, int64_t n2, int64_t modular = MOD) {
-  return ((n1 - n2) % modular + modular) % modular;
-}
-
-int64_t mul(int64_t n1, int64_t n2, int64_t modular = MOD) {
-  return ((n1 % modular) * (n2 % modular)) % modular;
-}
+/*
+// Chained Multiplication or Successive Simple Multiplication
+Modular<998244353> a=1, m=123456789;
+a *= m * m * m; // a = 519994069
+// Inverse
+a=inverse(m) // a=25170271
+// fractions
+Modular<> frac=(1,2); // frac=1*2^(-1) % 998244353 = 499122177
+// Modular exponentiation
+Modular<> power(2);
+power=mexp(power,500); // power = 616118644
+*/
